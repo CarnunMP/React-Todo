43d12c3892f776552743b80efd9bffb574c61266
CarnunMP <carnun@hotmail.co.uk>
2019-09-30 22:22:32 +0100

    build folder added

---
+++ build/precache-manifest.9907f810034358b8b2ff76fd64f13139.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1db487a45670db126d1d2b71da75fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dffaad5a7f50e21f4970",
+    "url": "/static/js/2.f76b1d44.chunk.js"
+  },
+  {
+    "revision": "aaccde9f6eeafdf0415f",
+    "url": "/static/js/main.bc5cebb7.chunk.js"
+  },
+  {
+    "revision": "470d86954b86ded07889",
+    "url": "/static/js/runtime-main.cbd3a812.js"
+  }
+]);
